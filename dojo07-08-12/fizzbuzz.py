1eff0b4ca615a235d9f41207a2e7bd7ada56579f
Alê Borba <alexandre.borba@imasters.com.br>
2012-08-07 20:22:50 -0300

    primeiro commit

---
+++ dojo07-08-12/fizzbuzz.py
@@ -0,0 +1,42 @@
+#coding: utf-8
+"""
+Neste problema, você deverá exibir uma lista de 1 a 100, um em cada linha, com as seguintes exceções:
+
+    Números divisíveis por 3 deve aparecer como 'Fizz' ao invés do número;
+    Números divisíveis por 5 devem aparecer como 'Buzz' ao invés do número;
+    Números divisíveis por 3 e 5 devem aparecer como 'FizzBuzz' ao invés do número'.
+
+"""
+
+import unittest
+
+def fizzbuzz(i):
+  if i % 3 == 0 :
+    return "Fizz"
+  elif i == 5:
+    return "Buzz"
+  return i
+
+
+class FBTest(unittest.TestCase):
+
+    def test_1(self):
+        self.assertEqual(fizzbuzz(1), 1)
+
+    def test_2(self):
+        self.assertEqual(fizzbuzz(2), 2)
+        
+    def test_3(self):
+        self.assertEqual(fizzbuzz(3), "Fizz")
+
+    def test_4(self):
+        self.assertEqual(fizzbuzz(4), 4)
+
+    def test_5(self):
+        self.assertEqual(fizzbuzz(5), "Buzz")
+    
+    def test_6(self):
+        self.assertEqual(fizzbuzz(6), "Fizz")
+        
+if __name__ == "__main__":
+    unittest.main()
