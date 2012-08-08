a684a6edc06e08ce884255aaf198dcfbc7c05d79
Alê Borba <alexandre.borba@imasters.com.br>
2012-08-07 21:06:51 -0300

    quinto elem da lista

--- dojo07-08-12/fizzbuzz.py
+++ dojo07-08-12/fizzbuzz.py
@@ -19,9 +19,11 @@ def fizzbuzz(i):
         return "Buzz"
     return i
     
-def lista_fizzbuzz(tamanho):
-    lista = range(1, tamanho + 1)
+def lista_fizzbuzz():
+    lista = range(1, 101)
     lista[2] = "Fizz"
+    lista[4] = "Buzz"
+
 
     
     return lista
@@ -29,22 +31,22 @@ def lista_fizzbuzz(tamanho):
 class FBListTest(unittest.TestCase):
 
     def test_obter_lista_tem_100_itens(self):
-        self.assertEqual(len(lista_fizzbuzz(100)), 100)
-
-    def test_obter_lista_tem_25_itens(self):
-        self.assertEqual(len(lista_fizzbuzz(25)), 25)
+        self.assertEqual(len(lista_fizzbuzz()), 100)
     
     def test_elemento_1_lista(self):
-        self.assertEqual(lista_fizzbuzz(100)[0], fizzbuzz(1))
+        self.assertEqual(lista_fizzbuzz()[0], fizzbuzz(1))
         
     def test_elemento_2_lista(self):
-        self.assertEqual(lista_fizzbuzz(100)[1], fizzbuzz(2))
+        self.assertEqual(lista_fizzbuzz()[1], fizzbuzz(2))
 
     def test_elemento_3_lista(self):
-        self.assertEquals(lista_fizzbuzz(100)[2], fizzbuzz(3))
+        self.assertEquals(lista_fizzbuzz()[2], fizzbuzz(3))
         
     def test_elemento_4_lista(self):
-        self.assertEquals(lista_fizzbuzz(100)[3], fizzbuzz(4))
+        self.assertEquals(lista_fizzbuzz()[3], fizzbuzz(4))
+
+    def test_elemento_5_lista(self):
+        self.assertEquals(lista_fizzbuzz()[4], fizzbuzz(5))
 
 class FBTest(unittest.TestCase):
 
