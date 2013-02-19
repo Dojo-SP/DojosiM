a0b30ae958b8f1f86ad535c611e07006b0cf8bad
Dojo <@>
2013-02-19 20:15:29 -0300

    Rogerio

--- jogovida.py
+++ jogovida.py
@@ -4,6 +4,18 @@ Created on Tue Feb 19 19:38:06 2013
 
 Jogo da Vida
 
+
+    Qualquer célula viva com menos que duas células vivas vizinhas morre, por baixa população;
+    Qualquer célula viva com mais que três células vivas vizinhas morre, por alta população;
+    Qualquer célula viva com duas ou três células vivas vizinhas permanece viva para a próxima geração;
+    Qualquer célula morta com exatamente três células vivas vizinhas se transforma em uma célula viva;
+
+Você também terá que pensar em:
+
+    Como representar a área do Jogo de uma maneira fácil de testar;
+    Que 'valor' as células fora da área do Jogo terão. Ou o Jogo não terá limite de área?
+
+
 @author: ---
 """
 
@@ -15,9 +27,12 @@ class M(object):
 
 def passo_vida(tabuleiro):
   if len(tabuleiro) == 2:
-    return [[M], [M]]
-  else:
-    return [[M]]
+    if len(tabuleiro[0]) == 2:
+      return  [[M, M],[M, M]]
+    else:
+      return [[M], [M]]
+  
+  return [[M]]
 
 class TestPassoVida(object): # unittes.TestCase
   
@@ -29,9 +44,8 @@ class TestPassoVida(object): # unittes.TestCase
 
   def test_uma_celula_morta(self):
     assert passo_vida([[M]]) ==  [[M]]
-  //Qualquer célula viva com menos que duas células vivas vizinhas morre, 
-  //por baixa população;
-  def teste_quatro_celulas(self):
+  
+  def teste_quatro_celulas_mortas(self):
     celulas = [[M, M],[M, M]]
-    assert passo_vida(celulas) ==  
+    assert passo_vida(celulas) ==  [[M, M],[M, M]]
 
