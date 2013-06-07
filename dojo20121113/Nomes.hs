d93763a2a72a3a905d194ec4017edf7a3722e662
Anderson Pavia <andersonaap@hotmail.com>
2013-06-07 08:13:51 -0300

    Create Nomes.hs

---
+++ dojo20121113/Nomes.hs
@@ -0,0 +1,108 @@
+module Nomes where
+
+--
+-- http://brasil.babycenter.com/pregnancy/nome/2011/ranking-mais-comuns/
+--
+
+nomesmeninas = 
+    [ ("Julia"             , '+')   --
+    , ("Sophia"            , '+')   --
+    , ("Isabella"          , '+')   --
+    , ("Maria Eduarda"     , '-')   --
+    , ("Manuela"           , '+')   --
+    , ("Giovanna"          , '+')   --
+    , ("Alice"             , '+')   --
+    , ("Laura"             , '+')   --
+    , ("Luiza"             , '+')   --
+    , ("Beatriz"           , '+')   --
+    , ("Mariana"           , '-')   --
+    , ("Yasmin"            , '+')   --
+    , ("Gabriela"          , '+')   --
+    , ("Rafaela"           , '+')   --
+    , ("Maria Clara"       , '-')   --
+    , ("Maria Luiza"       , '-')   --
+    , ("Ana Clara"         , '+')   --
+    , ("Isabelle"          , '+')   --
+    , ("Lara"              , '+')   --
+    , ("Ana Luiza"         , '+')   --
+    , ("Leticia"           , '+')   --
+    , ("Ana Julia"         , '+')   --
+    , ("Valentina"         , '+')   --
+    , ("Nicole"            , '+')   --
+    , ("Sarah"             , '+')   --
+    , ("Vitoria"           , '+')   --
+    , ("Isadora"           , '+')   --
+    , ("Livia"             , '+')   --
+    , ("Helena"            , '+')   --
+    , ("Ana Beatriz"       , '+')   --
+    , ("Lorena"            , '+')   --
+    , ("Clara"             , '+')   --
+    , ("Larissa"           , '+')   --
+    , ("Emanuelly"         , '+')   --
+    , ("Heloisa"           , '+')   --
+    , ("Marina"            , '+')   --
+    , ("Melissa"           , '+')   --
+    , ("Gabrielly"         , '+')   --
+    , ("Eduarda"           , '+')   --
+    , ("Maria Fernanda"    , '-')   --
+    , ("Rebeca"            , '+')   --
+    , ("Amanda"            , '+')   --
+    , ("Alicia"            , '+')   --
+    , ("Bianca"            , '+')   --
+    , ("Lavinia"           , '+')   --
+    , ("Fernanda"          , '+')   --
+    , ("Ester"             , '+')   --
+    , ("Carolina"          , '+')   --
+    , ("Emily"             , '+')   --
+    , ("Cecilia"           , '+')   --
+    , ("Maria Julia"       , '-')   --
+    , ("Pietra"            , '+')   --
+    , ("Ana Carolina"      , '+')   --
+    , ("Milena"            , '+')   --
+    , ("Marcela"           , '+')   --
+    , ("Lais"              , '+')   --
+    , ("Natalia"           , '+')   --
+    , ("Maria"             , '-')   --
+    , ("Bruna"             , '+')   --
+    , ("Camila"            , '+')   --
+    , ("Luana"             , '+')   --
+    , ("Ana Laura"         , '+')   --
+    , ("Catarina"          , '+')   --
+    , ("Maria Vitoria"     , '-')   --
+    , ("Maria Alice"       , '-')   --
+    , ("Olivia"            , '+')   --
+    , ("Agatha"            , '+')   --
+    , ("Mirella"           , '+')   --
+    , ("Sophie"            , '+')   --
+    , ("Stella"            , '+')   --
+    , ("Stefany"           , '+')   --
+    , ("Isabel"            , '+')   --
+    , ("Kamilly"           , '+')   --
+    , ("Elisa"             , '+')   --
+    , ("Luna"              , '+')   --
+    , ("Eloa"              , '+')   --
+    , ("Joana"             , '+')   --
+    , ("Mariane"           , '+')   --
+    , ("Barbara"           , '+')   --
+    , ("Juliana"           , '+')   --
+    , ("Rayssa"            , '+')   --
+    , ("Alana"             , '+')   --
+    , ("Ana Sophia"        , '+')   --
+    , ("Ana Livia"         , '+')   --
+    , ("Caroline"          , '+')   --
+    , ("Brenda"            , '+')   --
+    , ("Evelyn"            , '+')   --
+    , ("Debora"            , '+')   --
+    , ("Raquel"            , '+')   --
+    , ("Maite"             , '+')   --
+    , ("Ana"               , '+')   --
+    , ("Nina"              , '+')   --
+    , ("Maria Sophia"      , '-')   --
+    , ("Maria Cecilia"     , '-')   --
+    , ("Hadassa"           , '+')   --
+    , ("Antonella"         , '+')   --
+    , ("Jennifer"          , '+')   --
+    , ("Betina"            , '+')   --
+    , ("Mariah"            , '-')   --
+    , ("Sabrina"           , '+')   --
+    ]
