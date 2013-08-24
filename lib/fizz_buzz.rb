69b066a1fb3dcfd306aa532bad729f84a530a8eb
Dojo <@>
2013-08-24 12:05:31 -0300

    Solve for most multiples of 5

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,4 +1,5 @@
 def fizz_buzz_for_one_number number
   return "Fizz" if number % 3 == 0
+  return "Buzz" if number % 5 == 0
   number.to_s
 end
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -5,12 +5,18 @@ describe 'FizzBuzz' do
     it 'for a regular number, returns the number itself' do
       expect(fizz_buzz_for_one_number 1).to eq("1")
       expect(fizz_buzz_for_one_number 2).to eq("2")
+      expect(fizz_buzz_for_one_number 4).to eq("4")
     end
 
     it 'for a multiple of 3, returns "Fizz"' do
       expect(fizz_buzz_for_one_number 3).to eq("Fizz")
       expect(fizz_buzz_for_one_number 6).to eq("Fizz")
       expect(fizz_buzz_for_one_number 9).to eq("Fizz")
+    end
+
+    it 'for a multiple of 5, returns "Buzz"' do
+      expect(fizz_buzz_for_one_number 5).to eq("Buzz")
+      expect(fizz_buzz_for_one_number 10).to eq("Buzz")
     end
   end
 end
