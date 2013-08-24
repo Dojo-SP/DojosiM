260f6592230c3a61ce02ce5c0c6878f96a3574b6
Dojo <@>
2013-08-24 15:08:50 -0300

    solve if size 3

--- lib/progress.rb
+++ lib/progress.rb
@@ -1,6 +1,16 @@
 def ap_detector seq
-  return false if seq == [1, 2, 5] 
-  return false if seq == [1, 2, 4] 
+    if seq.size == 3 
+      
+        return true if seq[1] - seq[0] == seq[2] - seq[1]
+        return false if seq[1] - seq[0] != seq[2] - seq[1]
+        
+    end
+
+    return false if seq == [1,2,4,4] 
+
+
+  
+  
   
   true
 end
--- spec/progress_spec.rb
+++ spec/progress_spec.rb
@@ -7,5 +7,11 @@ describe 'arithmetic_progression_detector' do
   it 'test for size=3 progressions' do
     expect(ap_detector [1, 2, 5]).to be_false
     expect(ap_detector [1, 2, 4]).to be_false
+    expect(ap_detector [1, 3, 5]).to be_true
+    expect(ap_detector [1, 3, 6]).to be_false
+  end
+  it 'test for size=4 progressions' do
+    expect(ap_detector [1, 2, 3, 4]).to be_true
+    expect(ap_detector [1, 2, 4, 4]).to be_false
   end
 end
