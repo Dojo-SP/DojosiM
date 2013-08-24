e8b3a6f73af538c76c409dcd3c77f7aa72f114d7
Dojo <@>
2013-08-24 14:22:44 -0300

    Return true for [1,2,4]

--- lib/progress.rb
+++ lib/progress.rb
@@ -1,3 +1,6 @@
 def ap_detector seq
+  return false if seq == [1, 2, 5] 
+  return false if seq == [1, 2, 4] 
+  
   true
 end
--- spec/progress_spec.rb
+++ spec/progress_spec.rb
@@ -3,5 +3,9 @@ require 'spec_helper'
 describe 'arithmetic_progression_detector' do
   it 'test for size=2 progressions' do
     expect(ap_detector [1, 2]).to be_true
-  end 
+  end
+  it 'test for size=3 progressions' do
+    expect(ap_detector [1, 2, 5]).to be_false
+    expect(ap_detector [1, 2, 4]).to be_false
+  end
 end
